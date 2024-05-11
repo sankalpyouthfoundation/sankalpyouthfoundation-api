53c504a074a3d39d283dade6c8214b17c3831c12
techiemanish <manishtiwari627@gmail.com>
2024-05-11 11:42:27 +0530

    Contact Get All implemented

--- src/main/java/org/sankalpyouthfoundation/controller/ContactController.java
+++ src/main/java/org/sankalpyouthfoundation/controller/ContactController.java
@@ -5,12 +5,10 @@ import org.sankalpyouthfoundation.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 public class ContactController {
@@ -18,12 +16,18 @@ public class ContactController {
     @Autowired
     private ContactService contactService;
 
-    @PostMapping("/api/contact")
+    @PostMapping("/api/contacts")
     public ResponseEntity<Contact> saveRecord(@RequestBody Contact contact){
         LocalDateTime localDateTime = LocalDateTime.now();
         contact.setTimestamp(localDateTime);
         Contact result = this.contactService.save(contact);
         return ResponseEntity.status(HttpStatus.CREATED).body(result);
+    }
+
+    @GetMapping("/api/contacts")
+    public ResponseEntity<List<Contact>> getAllData(){
+        List<Contact> result = this.contactService.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
 }
