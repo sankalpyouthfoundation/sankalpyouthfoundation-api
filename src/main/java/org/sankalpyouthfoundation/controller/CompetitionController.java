9937647dc0684d44834fdb01a816a9a661292e8c
techiemanish <manishtiwari627@gmail.com>
2024-05-14 18:28:00 +0530

    Delete mapping added for competition

--- src/main/java/org/sankalpyouthfoundation/controller/CompetitionController.java
+++ src/main/java/org/sankalpyouthfoundation/controller/CompetitionController.java
@@ -6,12 +6,10 @@ import org.sankalpyouthfoundation.util.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class CompetitionController {
@@ -22,6 +20,7 @@ public class CompetitionController {
     @PostMapping("/api/competitions")
     public ResponseEntity<Object> saveCompetition(@RequestBody Competition competition){
         if(null != competition && null != competition.getTitle() && null != competition.getName()){
+            competition.setId(competition.getTitle().replace(" ", "")+competition.getName().replace(" ", ""));
             Competition result = this.competitionService.save(competition);
             return ResponseEntity.status(HttpStatus.OK).body(result);
         }
@@ -33,5 +32,19 @@ public class CompetitionController {
     public ResponseEntity<Object> getAllData(){
         List<Competition> result = this.competitionService.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    @DeleteMapping("/api/competitions")
+    public ResponseEntity<Object> deleteById(@RequestBody Competition competition){
+        Optional<Competition> data  = this.competitionService.findById(competition.getId());
+        if(data.isPresent()){
+            this.competitionService.deleteById(competition.getId());
+            Message message = new Message("Success", "Successfully deleted!");
+            return ResponseEntity.status(HttpStatus.OK).body(message);
+        }
+        else {
+            Message message = new Message("error", "No record found");
+            return ResponseEntity.status(HttpStatus.OK).body(message);
+        }
     }
 }
--- src/main/java/org/sankalpyouthfoundation/model/Competition.java
+++ src/main/java/org/sankalpyouthfoundation/model/Competition.java
@@ -3,11 +3,14 @@ package org.sankalpyouthfoundation.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Competition {
+    @Id
+    private String id;
     private String title;
     private String name;
     private String rank;
